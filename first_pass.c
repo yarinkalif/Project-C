d8386c8441fbb8bea5bef57d0105a4baf3f3ab0d
Maya Katan <135252531+MayaKatan@users.noreply.github.com>
2023-08-14 20:59:56 +0300

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -1,1 +1,28 @@
+#include "data.h"
+#include <stdlib.h>
+#include <string.h>
+
+/*IC and DC initialization*/
+int IC = 0;
+int DC = 0;
+
+/*Reading the next line from the source file.*/
+/* Function prototypes */
+void addSymbolToTable(const char* symbol, int address, SymbolType type);
+SymbolTable* findSymbolInTable(const char* symbol);
+
+#endif
+
+/*Turn on the flag "there is a symbol definition"*/
+symbolDefinitionFlag = 1;  
+
+if (symbolDefinitionFlag) {
+    /* Handle the case when there's a symbol definition */
+} else {
+    /* Handle the case when there's no symbol definition */
+}
+
+
+
+
 
