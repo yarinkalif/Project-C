3d7843515d4f0d598a56dcbe30df0fd880b9e754
Maya Katan <135252531+MayaKatan@users.noreply.github.com>
2023-08-26 12:43:44 +0300

    assembler.h

--- assembler.h
+++ assembler.h
@@ -1,14 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*#include "first_pass.h"*/
+/* 
+ * Header file for the main functionality of the assembly-to-machine code conversion.
+ */
+
+/* Libraries and headers for various functionalities required in the main conversion process:*/
+
+/* Include preassembler functions and structures */
 #include "preassembler.h"
+
+/* Include symbol table functions and structures */
 #include "table.h"
+
+/* Include global constants and variables */
 #include "globals.h"
+
+/* Include utility functions for general-purpose operations */
 #include "utils.h"
+
+/* Include error handling functions and constants */
 #include "errors.h"
 
-
+/* 
+ * Converts the provided assembly file to machine code.
+ * @param fileName: The name of the file to be processed.
+ * @return: Returns 1 if the process was successful, 0 otherwise.
+ */
 int assembly_to_machine_code(char* fileName);
 
 #endif
+
