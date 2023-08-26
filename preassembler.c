ce4557da766b151bcb73eb34bf7d7dcbfaa0de05
Maya Katan <135252531+MayaKatan@users.noreply.github.com>
2023-08-26 13:45:06 +0300

    preassembler.c

--- preassembler.c
+++ preassembler.c
@@ -5,8 +5,19 @@
 #include <string.h>
 #include "globals.h"
 
-
 #define MAX_MACRO_SIZE_NAME 40
+
+/**
+ * @brief Pre-assembles the provided assembly file.
+ * 
+ * This function reads an assembly file and performs macro substitution to produce an intermediate ".am" file.
+ * The function reads each line of the ".as" file and looks for macro definitions. If a macro is found, 
+ * it gets stored in a macro table. Once all macros have been identified, the function will then substitute 
+ * instances of these macros in the assembly file with their content.
+ * 
+ * @param fileName The name of the assembly file to be pre-assembled (without the ".as" extension).
+ * @return 1 if the pre-assembly process completes successfully; 0 otherwise.
+ */
 
 int preassembler_file(char* fileName) {
 	FILE *amFile; /*the file after the preassembler*/
