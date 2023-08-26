ad1e7920bc1a73b27d6e527acc820e140cf4f5c4
Maya Katan <135252531+MayaKatan@users.noreply.github.com>
2023-08-26 13:15:27 +0300

    binary.c

--- binary.c
+++ binary.c
@@ -4,8 +4,13 @@
 #include "globals.h"
 #include "utils.h"
 
-
-/*this function represent the line instruction in binary*/
+/**
+ * Represents the given assembly line instruction in binary format.
+ * @param machineCode: The output binary representation of the instruction.
+ * @param keyWord: Assembly instruction keyword (e.g., "MOV", "ADD").
+ * @param operands: The operands for the instruction.
+ * @param numberLine: The line number of the instruction in the assembly file.
+ */
 void line_instruction_binary(char *machineCode, char *keyWord, char *operands, int numberLine) {
 	int currOpcode = 0;
 	instructionGroup1 currLine1;
@@ -29,11 +34,11 @@ void line_instruction_binary(char *machineCode, char *keyWord, char *operands, i
 			int_to_binary(machineCode, currLine3.size.WORD);
 }
 
-
-
-
-
-/*identify the operand type*/
+/**
+ * Identifies the type of the operand (e.g., REGISTER, NUMBER, STRING, LABEL).
+ * @param operand: The operand string to identify.
+ * @return: The type of operand.
+ */
 OperandType identify_operand_type(char *operand) {
     if (operand[0] == '@' && operand[1] == 'r' && operand[2] >= '0' && operand[2] <= '7' && operand[3] == '\0') {
         return REGISTER;
@@ -46,8 +51,16 @@ OperandType identify_operand_type(char *operand) {
     }
 }
 
-/*this function create the instruction of group 1 - the group with two operands*/
-instructionGroup1 create_instruction_1(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
+/**
+ * Creates the instruction for group 1, which involves two operands.
+ * @param keyword: Assembly instruction keyword.
+ * @param operands: The operands for the instruction.
+ * @param currOpcode: The opcode for the instruction.
+ * @param typeOfKeyWord: The type of keyword.
+ * @param numberLine: The line number of the instruction.
+ * @return: The binary representation for group 1 instruction.
+ */
+	instructionGroup1 create_instruction_1(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
 	instructionGroup1 instr;
 	instr->instruction.opcode = currOpcode;
 
@@ -93,8 +106,16 @@ instructionGroup1 create_instruction_1(char *keyword, char *operands, int currOp
 	return instr;
 }	
 
-/*this function create the instruction of group 2 - the group with two operands*/
-instructionGroup2 create_instruction_2(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
+/**
+ * Creates the instruction for group 2, which involves two operands.
+ * @param keyword: Assembly instruction keyword.
+ * @param operands: The operands for the instruction.
+ * @param currOpcode: The opcode for the instruction.
+ * @param typeOfKeyWord: The type of keyword.
+ * @param numberLine: The line number of the instruction.
+ * @return: The binary representation for group 2 instruction.
+ */
+	instructionGroup2 create_instruction_2(char *keyword, char *operands, int currOpcode, int typeOfKeyWord, int numberLine) {
 	instructionGroup1 instr;
 	instr->instruction.opcode = currOpcode;
 
@@ -114,6 +135,14 @@ instructionGroup2 create_instruction_2(char *keyword, char *operands, int currOp
 	instr.instruction.str = 0;	
 }
 
+/**
+ * Creates the instruction for group 3, which doesn't involve operands.
+ * @param keyword: Assembly instruction keyword.
+ * @param currOpcode: The opcode for the instruction.
+ * @param typeOfKeyWord: The type of keyword.
+ * @param numberLine: The line number of the instruction.
+ * @return: The binary representation for group 3 instruction.
+ */	
 instructionGroup3 create_instruction_3(char *keyword, int currOpcode, int typeOfKeyWord, int numberLine) {
 	instructionGroup1 instr;
 	instr->instruction.opcode = currOpcode;
@@ -122,7 +151,14 @@ instructionGroup3 create_instruction_3(char *keyword, int currOpcode, int typeOf
 	instr.instruction.str = 0;
 }
 
-
+/**
+ * Represents the given assembly guidance in binary format.
+ * @param machineCode: The output binary representation.
+ * @param keyWord: Assembly guidance keyword (e.g., "DATA", "STRING").
+ * @param dataList: The data for the guidance.
+ * @param typeOfGuidence: The type of guidance.
+ * @param numberLine: The line number of the guidance in the assembly file.
+ */
 
 void line_guidence_binary(char *machineCode, char *keyWord, long dataList,int typeOfGuidence, int numberLine) {
 	DataCode currData; 
@@ -146,10 +182,12 @@ void line_guidence_binary(char *machineCode, char *keyWord, long dataList,int ty
 	} /*end of switch*/	
 }
 
-
-
-
-/*this function convert integer to binary represantation*/
+/**
+ * Converts the given integer into its binary representation.
+ * @param machineCode: The output binary string.
+ * @param curr: The integer to convert.
+ * @param numOfBit: The number of bits in the integer.
+ */
 void int_to_binary(char *machineCode, int curr, int numOfBit) {
 	unsigned mask = 1;
 
@@ -168,6 +206,12 @@ void int_to_binary(char *machineCode, int curr, int numOfBit) {
 
 static char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+/**
+ * Encodes a given string using Base64 encoding.
+ * @param input: The input string to encode.
+ * @param inputLength: The length of the input string.
+ * @return: The Base64 encoded string.
+ */
 
 char *base64_encode(unsigned char *input, size_t inputLength) {
 	size_t outputLength = 4*((inputLength+2)/3;
@@ -196,6 +240,11 @@ char *base64_encode(unsigned char *input, size_t inputLength) {
 	return encoded_data;
 }
 
+/**
+ * Encodes a 12-bit binary word into its Base64 representation.
+ * @param binaryWord: The 12-bit binary word.
+ * @return: The Base64 encoded representation of the word.
+ */
 char *encode_binary_word_to_base64(uint16_t binaryWord) {
 	/*convert the 12-bit binary word to 2 bytes*/
 	unsigned char bytes[2];
