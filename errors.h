64ac77be00b3ffe43aa4c2d36de2ea3bc1467a8a
Maya Katan <135252531+MayaKatan@users.noreply.github.com>
2023-08-26 13:32:13 +0300

    errors.h

--- errors.h
+++ errors.h
@@ -3,7 +3,20 @@
 
 #include "globals.h"
 
-
+/**
+ * @file errors.h
+ * This header file contains definitions and declarations related to error handling.
+ */
+/**
+ * @brief Handles error messages associated with reading and processing a source file.
+ * 
+ * This function takes an error code and the line number where the error occurred. Based on the given error code, 
+ * it prints a relevant error message, indicating the exact line in the source file that caused the error.
+ * 
+ * @param error_code    The error code corresponding to the detected error. Error codes are defined in "globals.h".
+ * @param line_counter  The line number in the source file where the error was detected.
+ */
 void handle_error(int error_code, int line_counter);
 
 #endif 
+
